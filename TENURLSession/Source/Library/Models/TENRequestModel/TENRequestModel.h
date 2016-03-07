0717309811335b0c7b8f78085e1d13c077e53d07
444ten <444ten@gmail.com>
2016-03-07 17:43:22 +0200

    TENRequestModel -> initial commit

---
+++ TENURLSession/Source/Library/Models/TENRequestModel/TENRequestModel.h
@@ -0,0 +1,13 @@
+//
+//  TENRequestModel.h
+//  TENURLSession
+//
+//  Created by 444ten on 3/7/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import "PDTModel.h"
+
+@interface TENRequestModel : PDTModel
+
+@end
---
+++ TENURLSession/Source/Library/Models/TENRequestModel/TENRequestModel.m
@@ -0,0 +1,13 @@
+//
+//  TENRequestModel.m
+//  TENURLSession
+//
+//  Created by 444ten on 3/7/16.
+//  Copyright © 2016 444ten. All rights reserved.
+//
+
+#import "TENRequestModel.h"
+
+@implementation TENRequestModel
+
+@end
